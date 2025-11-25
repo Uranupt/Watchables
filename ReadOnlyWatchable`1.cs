25a31ccb8f5a2eb7fd04f5107b225aefe3a9b42d
Ross Allan <31083803+Uranupt@users.noreply.github.com>
2025-11-25 09:26:28 -0800

    Update ReadOnlyWatchable`1.cs
    Temp Update

--- ReadOnlyWatchable`1.cs
+++ ReadOnlyWatchable`1.cs
@@ -7,32 +7,24 @@ namespace Watchables
 
 		private readonly Watchable<T> _baseValue;
 
-		public override Watchable<T> ReadOnlyWrapper
-		{
-			get
-			{
-				return this;
-			}
-		}
+		public override IsOwned => _baseValue.IsOwned;
+		public override IsDestroyed => _baseValue.IsDestroyed;
 
 		public ReadOnlyWatchable(Watchable<T> baseValue)
 		{
 			_baseValue = baseValue;
+			_readOnlyWrapper = this;
 		}
 
-		public override T ToValue()
-		{
-			return _baseValue;
-		}
+		public override T ToValue() => baseValue.ToValue();
 
-		public override void AddListener(Action listener)
+		private void OnChanged() => InvokeChanged();
+		
+		private void OnDestroyed()
 		{
-			_baseValue.AddListener(listener);
-		}
-
-		public override void RemoveListener(Action listener)
-		{
-			_baseValue.RemoveListener(listener);
+			_baseValue.Changed -= OnChanged();
+			_baseValue.Destroyed -= OnDestroyed();
+			
 		}
 
 	}

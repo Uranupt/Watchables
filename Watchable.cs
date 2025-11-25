fbf11121e4f87cfee91b2d19456ac8a212a28b2b
Ross Allan <raknosvonsarrgoth@gmail.com>
2025-11-25 06:34:17 -0800

    In Prog Sync 1

--- Watchable.cs
+++ Watchable.cs
@@ -3,27 +3,79 @@ using System;
 
 namespace Watchables
 {
-
-	public abstract class Watchable<T>
+	/// <summary>
+	/// Base class for the Watchable system of subscribable classes. 
+	/// Provides the core <see cref="Changed"/> and <see cref="Destroyed"/> events, as well as ownership logic.
+	/// </summary>
+	public abstract class Watchable
 	{
 
-		protected T _value;
-		public abstract Watchable<T> ReadOnlyWrapper { get; }
+		private object _owner;
 
-		public static implicit operator T(Watchable<T> watchable) => watchable.ToValue();
+		/// <summary> 
+		/// Whether this <see cref="Watchable"/> currently has a defined owner. If this is true, the owner is required to
+		/// call <see cref="TryDestroy"/> and <see cref="TryClearListeners"/>.
+		/// </summary>
+		public bool IsOwned => _owner != null;
+		/// <summary> Whether this <see cref="Watchable"/> has already been destroyed. </summary>
+		public bool IsDestroyed { get; private set; } = false;
+    /// <summary> The event fired when the underlying value changes. </summary>
+    public event Action Changed;
+    /// <summary> The event fired when <see cref="TryDestroy"/> is successfully called. </summary>
+    public event Action<Watchable> Destroyed;
 
-		public abstract void AddListener(Action listener);
-
-		public abstract void RemoveListener(Action listener);
-
-		public virtual T ToValue()
+		/// <summary>
+		/// Attempts to clear the <see cref="Watchable"/>'s value and listeners and mark it destroyed. 
+		/// Requires the owner to be passed if <see cref="IsOwned"/> is true. Calls <see cref="Destroyed"/> when done.
+		/// </summary>
+		public bool TryDestroy(object owner = null)
 		{
-			return _value;
+			if((_owner != null && owner != _owner) || IsDestroyed) { return false; }
+			_owner = null;
+			IsDestroyed = true;
+			ClearValue();
+			TryClearListeners(owner);
+			Destroyed?.Invoke(this);
+			Destroyed = null;
+			return true;
 		}
 
-		public new virtual string ToString()
+		/// <summary>
+		/// Attempts to set the <see cref="Watchable"/>'s owner. Only succeeds if there is no current owner.
+		/// </summary>
+		public bool TrySetOwner(object owner)
 		{
-			return ToValue().ToString();
+			if(_owner != null || owner == null){ return false; }
+			_owner = owner;
+			return true;
 		}
+
+		/// <summary>
+		/// Attempts to clear the <see cref="Watchable"/>'s owner. Requires the current owner to be passed.
+		/// </summary>
+		public bool TryClearOwner(object owner)
+		{
+			if(_owner != owner) { return false; }
+			_owner = null;
+			return true;
+		}
+
+    /// <summary>
+    /// Attempts to clear all listeners from <see cref="Changed"/>. Requires the owner to be passed if <see cref="IsOwned"/> is true.
+    /// </summary>
+    public bool TryClearListeners(object owner = null)
+		{
+      if(_owner != null && _owner != owner) { return false; }
+			Changed = null;
+			return true;
+    }
+
+		/// <summary> Converts the underlying value to a string. </summary>
+		public new abstract string ToString();
+
+		protected abstract void ClearValue();
+
+		protected void InvokeChanged() => Changed?.Invoke();
+
 	}
 }
--- Watchable`1.cs
+++ Watchable`1.cs
@@ -3,22 +3,27 @@
 namespace Watchables
 {
 
+	/// <summary>
+	/// Generic type extension base of <see cref="Watchable"/>. 
+	/// Provides implicit casting to <typeparamref name="T"/> and access to a wrapper <see cref="ReadOnlyWatchable{T}"/>.
+	/// </summary>
 	public abstract class Watchable<T> : Watchable
 	{
 
+		private ReadOnlyWatchable<T> _readOnlyWrapper;
+
 		protected T _value;
-		public abstract Watchable<T> ReadOnlyWrapper { get; }
+
+		/// <summary> A read-only version of this <see cref="Watchable{T}"/>. </summary>
+		public ReadOnlyWatchable<T> ReadOnlyWrapper => _readOnlyWrapper ??= new(this);
 
 		public static implicit operator T(Watchable<T> watchable) => watchable.ToValue();
 
-		public virtual T ToValue()
-		{
-			return _value;
-		}
+		/// <summary> Retrieve the underlying value of Type <typeparamref name="T"/>. </summary>
+		public virtual T ToValue() => _value;
+		public override string ToString() => ToValue().ToString();
 
-		public override string ToString()
-		{
-			return ToValue().ToString();
-		}
-	}
+		protected override void ClearValue() => _value = default;
+
+  }
 }
